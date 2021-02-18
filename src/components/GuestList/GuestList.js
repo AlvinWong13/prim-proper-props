025e96aa272c8696496b3b3284b526182b6eb710
Alvin Wong <72238504+AlvinWong13@users.noreply.github.com>
2021-02-17 22:11:22 -0600

    Update GuestList.js
    delete button and guestitem

--- src/components/GuestList/GuestList.js
+++ src/components/GuestList/GuestList.js
@@ -1,4 +1,6 @@
-function guestList({guestList}) {
+import GuestItem from '../GuestItem/GuestItem'
+
+function guestList({guestList, removeGuest}) {
   return( 
     <div>
       <h2>Guest List</h2>
@@ -7,14 +9,16 @@ function guestList({guestList}) {
             <tr>
               <th>Name</th>
               <th>Kid's Meal</th>
+              <th>Remove Guest</th>
             </tr>
           </thead>
           <tbody>
             {guestList.map(guest => (
-              <tr key={guest.id}>
-                <td>{guest.name}</td>
-                <td>{String(guest.kidsMeal)}</td>
-              </tr>
+              <GuestItem
+              key={guest.id}
+              guest={guest}
+              removeGuest={removeGuest}
+              />
             ))}
           </tbody>
         </table>
